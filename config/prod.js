5167a7296db35fbe937627e42dc750d7060d9be4
Raja Raghav <raja_raghav@hotmail.com>
2018-08-10 21:50:05 +0530

    env variables and production config added

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,8 @@
+let dotenv = require("dotenv");
+dotenv.load();
+/* eslint-disable no-undef*/
+console.log(process.env.REDIS_HOST);
 module.exports = {
-	redisHost: "localhost",
-	redisPort: 5001
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT
 };
