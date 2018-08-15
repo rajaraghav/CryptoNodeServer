34735bda8c48bf0acac9e0e07cc1a0c9e3ffc33a
Raja Raghav <raja_raghav@hotmail.com>
2018-08-15 12:38:50 +0530

    added top 50 coins

--- services/SubsList/CCCAGG-subs.js
+++ services/SubsList/CCCAGG-subs.js
@@ -1,10 +1,11 @@
 let coins = require("./coins").default;
-let CCCAGGstring = "5~CCCAGG~BTC~";
+
+let CCCAGGstring = "5~CCCAGG~";
+let USDSTRING = "~USD";
 let cccaggBTCarr = [];
 for (let coin of coins) {
 
-	cccaggBTCarr.push(CCCAGGstring + coin);
+	cccaggBTCarr.push(CCCAGGstring + coin + USDSTRING);
 
 }
-
 module.exports = cccaggBTCarr;
