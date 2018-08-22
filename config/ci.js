9a0b35cc13afedb801fd3c7b21cac3db9a47453d
Raja Raghav <raja_raghav@hotmail.com>
2018-08-22 14:59:54 +0530

    added env variables

---
+++ config/ci.js
@@ -0,0 +1,5 @@
+module.exports = {
+	cookieKey: process.env.COOKIE_KEY,
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT
+};
--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,10 @@ if (process.env.NODE_ENV === "production") {
 
 	module.exports = require("./prod");
 
+} else if (process.env.NODE_ENV === "ci") {
+
+	module.exports = require("./prod");
+
 } else {
 
 	module.exports = require("./dev");
