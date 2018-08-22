a1347fc16a3aaf92ab1c31af223da3e552202a2f
Raja Raghav <raja_raghav@hotmail.com>
2018-08-22 15:28:54 +0530

    added mongo conf var

--- config/ci.js
+++ config/ci.js
@@ -1,6 +1,7 @@
 /* eslint-disable*/
 module.exports = {
 	cookieKey: process.env.COOKIE_KEY,
+	mongoURI: process.env.MONGO_URI,
 	redisHost: process.env.REDIS_HOST,
 	redisPort: process.env.REDIS_PORT
 };
