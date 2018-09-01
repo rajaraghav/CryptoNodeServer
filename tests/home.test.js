59d21be17ff570762504fea13c75814dba986c1c
Raja Raghav <raja_raghav@hotmail.com>
2018-09-01 19:28:19 +0530

    moved login test down

--- tests/home.test.js
+++ tests/home.test.js
@@ -9,19 +9,19 @@ test("server running", async () => {
 	let serverRep = await page.getContentOf("body");
 	console.log(serverRep);
 });
+test("signup works", async () => {
+	let rand = Math.round(Math.random() * 20000);
+	let email = "raja@aarus.com";
+	let password = "password";
+	let serverRep = await page.post("/signup", {
+		email,
+		password
+	});
+});
 test("login works", async () => {
 	let serverRep = await page.post("/login", {
 		email: "raja@aarus.com",
 		password: "password"
-	});
-});
-test("signup works", async () => {
-	let rand = Math.round(Math.random() * 20000);
-	let email = "raasda@aarus.com".concat(rand.toString());
-	let password = "password".concat(rand.toString());
-	let serverRep = await page.post("/signup", {
-		email,
-		password
 	});
 });
 
