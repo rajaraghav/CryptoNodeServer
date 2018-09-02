5c3d68ec879cd1989e89422263319424ebfd0210
Raja Raghav <raja_raghav@hotmail.com>
2018-09-02 16:50:22 +0530

    fixed travis bug

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,6 @@ module.exports = {
 	cookieKey: process.env.COOKIE_KEY,
 	mongoURI: process.env.MONGO_URI,
 	redisHost: process.env.REDIS_HOST,
-	redisPort: process.env.REDIS_PORT
+	redisPort: process.env.REDIS_PORT,
 	jwtKey: process.env.JWT_KEY
 };
