7de49ffb0b9076bbb77d87c394a8a716a1188e5a
Raja Raghav <raja_raghav@hotmail.com>
2018-10-09 22:54:46 +0530

    pulling

--- services/xServices/Xchange/ws-run-method.js
+++ services/xServices/Xchange/ws-run-method.js
@@ -1,65 +1,90 @@
-
 import { checkMethod } from "./check-method";
 
-const WebSocket = require('ws');
-let call_id = 0; 
-let ws_url = 'ws://localhost:8888';
-let ws = null; 
+const WebSocket = require("ws");
+let callId = 0;
+let wsUrl = "ws://localhost:8888";
+let ws = null;
+function start_ws (onready, onmessage) {
 
-function start_ws(onready, onmessage) {
-    var created_ws = false;
-    if (ws == null || ws.readyState != 1) {
-        ws = new WebSocket(ws_url);
-        created_ws = true;
-    }
-    ws.on('open' , function (event) {
-        console.log("opened ws to '" + ws_url + "'");
-        onready(ws);
-    })
-    ws.on('message' , function(event) {
-        onmessage(ws, event);
-    })
-    ws.on('error' , function(event) {
-        console.log("ws error (" + event + ")");
-    });
-    ws.on('close' , function(event) {
-        console.log("ws closed (" + event.code + ")");
-    });
-    if (!created_ws)
-        onready(ws);
+	var created_ws = false;
+	if (ws == null || ws.readyState != 1) {
+
+		ws = new WebSocket(wsUrl);
+		created_ws = true;
+
+	}
+	ws.on("open", (event) => {
+
+		console.log(`opened ws to '${wsUrl}'`);
+		onready(ws);
+
+	});
+	ws.on("message", (event) => {
+
+		onmessage(ws, event);
+
+	});
+	ws.on("error", (event) => {
+
+		console.log(`ws error (${event})`);
+
+	});
+	ws.on("close", (event) => {
+
+		console.log(`ws closed (${event.code})`);
+
+	});
+	if (!created_ws) {
+
+		onready(ws);
+
+	}
+
 }
 
-function call_ws_server(payload) {
-    call_id++;
-    let params = payload.params;
-    let method = payload.method;
-    console.log(method);
-    console.log(params);
-    start_ws(
-        function (sock) {
-            var req = JSON.stringify({id: call_id, method: method, params: params});
-            sock.send(req);
-        },
-        function (sock, data) {
-            console.log(data);
-           // var obj = JSON.parse(data);
-          //  console.log(obj);
-        }
-    );
+function call_ws_server (payload) {
+
+	callId++;
+	let params = payload.params;
+	let method = payload.method;
+	console.log(method);
+	console.log(params);
+	return start_ws(
+		(sock) => {
+
+			var req = JSON.stringify({
+				id: callId,
+				method,
+				params
+			});
+			sock.send(req);
+
+		},
+		(sock, data) => {
+
+			console.log(data);
+
+		}
+	);
+
 }
 
+export const wsRunMethod = async (
+	method,
+	_payload,
+	role = "public",
+	opts = {}
+) => {
 
-export const wsRunMethod = async (method, _payload, role = 'public', opts = {}) => {
-  console.log(method,_payload,role,opts)
-  const params = checkMethod(method, _payload, role, opts)
+	console.log(method, _payload, role, opts);
+	const params = checkMethod(method, _payload, role, opts);
 
-  const payload = {
-    method,
-    params: params || []
-  }
-
+	const payload = {
+		method,
+		params: params || []
+	};
 	console.log("payload", payload);
 
-  return call_ws_server(payload)
-    .then(json => JSON.parse(json.text))
-}
+	return call_ws_server(payload).then((json) => JSON.parse(json.text));
+
+};
