7a4b4320e72cc9631ac20beb549e852f09fab9da
dev1644 <soham.tomar@example.com>
2018-10-30 03:49:05 +0530

    Added Websocket Error Handler and tested for the public endpoints

--- services/xServices/Xchange/ws-run-method.js
+++ services/xServices/Xchange/ws-run-method.js
@@ -1,106 +1,82 @@
 const WebSocket = require("ws");
 let callId = 0;
 let wsUrl = "ws://localhost:8888";
+import { checkMethod } from "./ws-check-method";
+
 if (process.env.NODE_ENV === "production") {
-
-	wsUrl = "ws://localhost/xws";
-
+  wsUrl = "ws://localhost/xws";
 }
 let ws = null;
 let socketServer = null;
 let startWs = (onready, onmessage) => {
+  var createdWs = false;
+  if (ws === null || ws.readyState !== 1) {
+    ws = new WebSocket(wsUrl);
+    createdWs = true;
+  }
 
-	let createdWs = false;
-	if (ws === null || ws.readyState !== 1) {
-
-		ws = new WebSocket(wsUrl);
-		createdWs = true;
-
-	}
-	ws.on("open", (event) => {
-
-		console.log(`opened ws to '${wsUrl}'`, event);
-		onready(ws);
-
-	});
-	ws.on("message", (event) => {
-
-		onmessage(event);
-
-	});
-	ws.on("error", (event) => {
-
-		console.log(`ws error (${event})`);
-
-	});
-	ws.on("close", (event) => {
-
-		console.log(`ws closed (${event.code})`);
-
-	});
-	if (!createdWs) {
-
-		onready(ws);
-
-	}
-
+  ws.on("open", event => {
+    console.log(`opened ws to '${wsUrl}'`, event);
+    onready(ws);
+  });
+  ws.on("message", event => {
+    onmessage(event);
+  });
+  ws.on("error", event => {
+    console.log(`ws error (${event})`);
+  });
+  ws.on("close", event => {
+    console.log(`ws closed (${event.code})`);
+  });
+  if (!createdWs) {
+    onready(ws);
+  }
 };
-const callWsServer = (payload) => {
+let callWsServer = payload => {
+  callId += 1;
+  let { _params, method, role, socketId } = payload;
+  startWs(
+    sock => {
+      let params = checkMethod(method, _params, role);
 
-	callId += 1;
-	let { params, method, socketId } = payload;
-	startWs(
-		(sock) => {
-
-			let req = JSON.stringify({
-				id: callId,
-				method,
-				params
-			});
-			sock.send(req);
-
-		},
-		(data) => {
-
-			console.log(data);
-			socketServer.to(socketId).emit("xresponse", data);
-
-		}
-	);
-
+      var req = JSON.stringify({
+        id: callId,
+        method,
+        params
+      });
+      console.log(req);
+      sock.send(req);
+    },
+    data => {
+      socketServer.to(socketId).emit("xresponse", data);
+    }
+  );
 };
 /* eslint-disable max-params*/
 export const wsRunMethod = (
-	method,
-	params,
-	socketId,
-	role = "public",
-	opts = {}
+  method,
+  _params,
+  socketId,
+  role = "public",
+  opts = {}
 ) => {
+  /* eslint-enable max-params*/
+  console.log(method, _params, socketId, role, opts);
 
-	/* eslint-enable max-params*/
-	console.log(method, params, socketId, role, opts);
-
-	const payload = {
-		method,
-		params: params || [],
-		socketId
-	};
-	callWsServer(payload);
-
+  const payload = {
+    method,
+    _params: _params || [],
+    role,
+    socketId
+  };
+  callWsServer(payload);
 };
-export const xchangeWs = (io) => {
-
-	socketServer = io;
-	socketServer.on("connection", (socket) => {
-
-		socket.on("wsx", (dat) => {
-
-			console.log(dat);
-			wsRunMethod(dat.method, dat.params, socket.id);
-
-		});
-
-	});
-
+export const xchangeWs = io => {
+  socketServer = io;
+  socketServer.on("connection", socket => {
+    socket.on("wsx", dat => {
+      console.log(dat);
+      wsRunMethod(dat.method, dat.params, socket.id, dat.role);
+    });
+  });
 };
--- wsdemo.js
+++ wsdemo.js
@@ -2,13 +2,13 @@ const io = require("socket.io-client");
 const ioClient = io.connect("http://localhost:9000");
 ioClient.on("xresponse", msg => console.info(msg));
 
-ioClient.emit("wsx", {
-  method: "server.auth",
-  params: [
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAZ21haWwuY29tIiwiaWQiOiI1YmIwZmUxZGY2YjBhZTQ3MzI2NTk0YTIiLCJpYXQiOjE1Mzk0NDg0OTksImV4cCI6MTUzOTYyMTI5OX0.M8zuKJICVdC1JMGeXM76tsjcW6ALaJCtE00LujMOVas",
-    "web"
-  ]
-});
+// ioClient.emit("wsx", {
+//   method: "server.auth",
+//   params: [
+//     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAZ21haWwuY29tIiwiaWQiOiI1YmIwZmUxZGY2YjBhZTQ3MzI2NTk0YTIiLCJpYXQiOjE1Mzk0NDg0OTksImV4cCI6MTUzOTYyMTI5OX0.M8zuKJICVdC1JMGeXM76tsjcW6ALaJCtE00LujMOVas",
+//     "web"
+//   ]
+// });
 
 // ioClient.emit("wsx", {
 //   method: "order.query",
@@ -17,5 +17,12 @@ ioClient.emit("wsx", {
 
 // ioClient.emit("wsx", {
 //   method: "price.query",
-//   params: ["ETHBTC"]
+//   params: { market: "ETHBTC" },
+//   role: "public"
 // });
+
+ioClient.emit("wsx", {
+  method: "today.query",
+  params: { market: "ETHBTC" },
+  role: "public"
+});
